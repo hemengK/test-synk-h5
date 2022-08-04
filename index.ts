25cfdd8f9781145c581a48ea5f196fe31dacb487
hemeng <517505440@QQ.COM>
2022-08-04 17:04:05 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { get } from '@/utils/http-request';
 
 const GRANT_TYPE = 'qwei_xin_free_login';
 const CLIENT_ID = '2b01a310eafd47f1a8dd41b14da0dd3a';
-const CLIENT_SECRET =
+const CLIENT =
   'eOCEY690UgvIl94W0G1bs4PNOD9J6Qaod3wM704HggMRFPJp9BZLLFUgQN3MLYQM';
 
 const authUrl = getAuthUrl();
@@ -20,6 +20,6 @@ export const userAuth = (code: string) => {
     code: code,
     grant_type: GRANT_TYPE,
     client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
+    client_secret: CLIENT,
   });
 };
